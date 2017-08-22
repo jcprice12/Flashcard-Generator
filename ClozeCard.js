5658420929fdbfa327bd334d0b8dd80e562b96ea
jcprice12 <jcprice12@gmail.com>
2017-08-21 22:55:00 -0400

    fixed bugs search finds first match and returns

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,22 +7,33 @@ function ClozeCard(text, cloze){
     console.log(this.startIndexes);
 }
 ClozeCard.prototype.search = function(sent, index, cloze, builtCloze, searchIndex){
+    console.log("start of function (" + sent + "," + index + "," + cloze + "," + builtCloze + "," + searchIndex + ")");
     if(builtCloze === cloze){
+        console.log("found");
         return true;
     } else {
-        if(cloze.length <= (sent.length - index)){
+        if(cloze.length <= (sent.length - (index - (cloze.length - 1)))){
+            console.log("not found yet");
             if(sent[index] === cloze[searchIndex]){
+                console.log("sent[" + index + "] === cloze[" + searchIndex + "]");
                 builtCloze = builtCloze + sent[index];
+                console.log("builtCloze: " + builtCloze);
                 if(this.search(sent,(index + 1), cloze, builtCloze, (searchIndex + 1))){
+                    console.log("found and pushing");
                     this.startIndexes.push(index - (cloze.length - 1));
                 }
+            } else {
+                this.search(sent, ((index + 1) - builtCloze.length), cloze, "", 0);
             }
         } else {
+            console.log("not found at all");
             return false;
         }
     }
-    if(sent[(index + 1)] === cloze[0]){
-        this.search(sent, (index + 1), cloze, sent[(index + 1)], 1);
-    }
+    // if((index + 1) < sent.length){
+    //     if(sent[index] === cloze[0]){
+    //         this.search(sent, (index + 1), cloze, sent[index], 1);
+    //     }
+    // }
 }
 module.exports = ClozeCard;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
 
-var clozeCard = new ClozeCard("George Washington was the first president", "George Washington");
-var clozeCard = new ClozeCard();
+var clozeCard = new ClozeCard("Cool Geor George Washington was the first president", "George Washington");
+var clozeCard = new ClozeCard("iim", "im");
