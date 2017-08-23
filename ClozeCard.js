1c005cc51c6ddac13ec68f00b789f5eb7d519933
jcprice12 <jcprice12@gmail.com>
2017-08-22 20:40:35 -0400

    added ability to choose to find first occurence or find all occurences of cloze in full text

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,12 +2,24 @@ function replaceAt(baseStr, index, replacement) {
     return baseStr.substr(0, index) + replacement + baseStr.substr(index + replacement.length);
 }
 
-function ClozeCard(text, cloze){
+function ClozeCard(text, cloze, multi){
     this.fullText = text;
     this.cloze = cloze;
     this.partial = null;
-    if(!this.search(this.fullText, 0, this.cloze, "", 0, this.fullText)){
-        throw (this.cloze + " could not be found in " + this.fullText);
+    if(this.fullText && this.cloze){
+        if(!multi){
+            if(!this.search(this.fullText, 0, this.cloze, "", 0, this.fullText)){
+                this.errorCodes["101"](this.cloze, this.fullText);
+            }
+        } else {
+            this.multiSearch(this.fullText, this.cloze, this.fullText);
+            if(this.partial === this.fullText){
+                this.partial = null;
+                this.errorCodes["101"](this.cloze, this.fullText);
+            }
+        }
+    } else {
+        this.errorCodes["102"]();
     }
 }
 ClozeCard.prototype.search = function(sent, index, cloze, builtCloze, searchIndex, partial){
@@ -27,6 +39,35 @@ ClozeCard.prototype.search = function(sent, index, cloze, builtCloze, searchInde
             this.partial = null;
             return false;
         }
+    }
+}
+ClozeCard.prototype.multiSearch = function(sent, cloze, partial){
+    for(var i = 0; i < sent.length; i++){
+        if(cloze.length <= (sent.length - (i - (cloze.length - 1)))){
+            var j = 0;
+            var oldPartial = partial;
+            for(j; j < cloze.length; j++){
+                if(cloze[j] !== sent[(i+j)]){
+                    partial = oldPartial;
+                    j = 0;
+                    break;
+                } else {
+                    partial = replaceAt(partial, (i+j), "_")
+                }
+            }
+            i = i + j;
+        } else {
+            break;
+        }
+    }
+    this.partial = partial;
+}
+ClozeCard.prototype.errorCodes = {
+    "101": function(cloze, fullText){
+        throw ("\"" + cloze + "\" could not be found in \"" + fullText + "\"");
+    },
+    "102": function(){
+        throw ("Full Text and Cloze cannot be empty, null, or undefined");
     }
 }
 module.exports = ClozeCard;
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
 
+// try {
+//     var clozeCard1 = new ClozeCard("Cool Geor George Washington was the first president", "George Washington", false);
+//     console.log(clozeCard1);
+// } catch(error){
+//     console.log(error);
+// }
+
+// try{
+//     var clozeCard2 = new ClozeCard("iiiiiiiiiiiiii", "im", false);
+//     console.log(clozeCard2);
+// } catch(error){
+//     console.log(error);
+// }
+
 try {
-    var clozeCard1 = new ClozeCard("Cool Geor George Washington was the first president", "George Washington");
-    console.log(clozeCard1);
+    var clozeCard3 = new ClozeCard("Cool Geor George Washington was the first president", "George Washington", true);
+    console.log(clozeCard3);
 } catch(error){
     console.log(error);
 }
 
 try{
-    var clozeCard2 = new ClozeCard("iiiiiiiiiiiiii", "im");
-    console.log(clozeCard2);
+    var clozeCard4 = new ClozeCard("iiiiiiiiiiiiii", "im", true);
+    console.log(clozeCard4);
+} catch(error){
+    console.log(error);
+}
+
+try{
+    var clozeCard5 = new ClozeCard("he George was the 1st president. he George crossed the river", "George", false);
+    console.log(clozeCard5);
 } catch(error){
     console.log(error);
 }
