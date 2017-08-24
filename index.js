d4c49011e655137cb80239f9d289d5d5d994b1d0
jcprice12 <jcprice12@gmail.com>
2017-08-24 18:54:43 -0400

    added small console log

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("There is no command-line interface for this app at the moment");
 console.log("To run some tests, type 'npm test' while in the root folder");
+console.log("Tests can be found in ./test/test-cards.js");
